8a4372c4e52f81bcaca89b84597d9e5bc648ec5a
djo2008 <musicworkstation2008@gmail.com>
2024-06-23 11:09:48 +0100

    Test

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes , Route } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import DetailMovie from "./components/DetailMovie";
+
+
+function App () {
+
+  return (
+    <>
+    <Navbar></Navbar>
+    <Routes>
+        <Route path="/" element={<Home></Home>} ></Route>
+        <Route path="/view/:id" element={<DetailMovie></DetailMovie>} ></Route>
+        <Route path="/Contact" element={<Contact></Contact>}></Route>
+    </Routes>
+
+    </>
+  )
+}
+export default App;
---
+++ src/Movies.js
@@ -0,0 +1,32 @@
+var Movies = [
+    {id : "1",
+     title:"spiderman",
+     description:"best movie",
+     postUrl:"https://res.cloudinary.com/duc20y5dw/image/upload/v1716111655/spiderman_zlczhz.jpg",
+     VideoLink : "https://www.youtube.com/embed/t06RUxPbp_c?si=0cCVOyWatSBdoP4Z",
+     rating:"8",
+
+    },
+
+    { id : "2",
+      title:"batman",
+      description:"best movie",
+      postUrl:"https://res.cloudinary.com/duc20y5dw/image/upload/v1716111800/Batman__black_background_bdoz8o.jpg",
+      VideoLink : "https://www.youtube.com/embed/GRbd7l_niEY?si=r0AIVX7kz1jLGan2",
+      rating:"5",
+
+   },
+
+   {id : "3",
+    title:"maze runner",
+    description:"best movie",
+    postUrl:"https://res.cloudinary.com/duc20y5dw/image/upload/v1716111952/MV5BMjUyNTA3MTAyM15BMl5BanBnXkFtZTgwOTEyMTkyMjE_._V1_FMjpg_UX1000__vvd59h.jpg",
+    VideoLink : "https://www.youtube.com/embed/AwwbhhjQ9Xk?si=0QzIRGNCDxNeIYC5",
+    rating:"10"
+
+  }
+  
+
+
+]
+export default Movies;
